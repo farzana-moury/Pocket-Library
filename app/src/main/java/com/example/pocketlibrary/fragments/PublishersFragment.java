bede23699cf227018cf36e1feaee1942c124d65f
Farzana Moury <w0737626@myscc.ca>
2021-04-06 16:43:32 -0400

    #10 ViewPager2 Class    * PublishersFragment    * created ViewPager2      adapter class    * linked to view object

--- app/src/main/java/com/example/pocketlibrary/fragments/PublishersFragment.java
+++ app/src/main/java/com/example/pocketlibrary/fragments/PublishersFragment.java
@@ -2,7 +2,11 @@ package com.example.pocketlibrary.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,8 +29,47 @@ public class PublishersFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_publishers, container, false);
 
-
+        CustomViewPager2Adapter adapter = new CustomViewPager2Adapter(getActivity()); //making the adapter our custom adapter
+        ViewPager2 viewPager = view.findViewById(R.id.publishers); //finding our viewpager
+        viewPager.setAdapter(adapter); //setting the adapter for the viewpager
 
         return view;
+    }
+
+    /**
+     * Customn ViewPager2 adapter
+     *
+     * @author Farzana Moury
+     * @since April 6 2021
+     * @version 1.0
+     */
+    public class CustomViewPager2Adapter extends FragmentStateAdapter{
+
+        /**
+         * constructor
+         * @param fm fragment manager
+         */
+        public CustomViewPager2Adapter(@NonNull FragmentActivity fm) {
+            super(fm);
+        }
+
+        //implemented methods
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch(position){
+                case 0: return PublisherFragment.newInstance("Publisher1", 0, "Description1", "url");
+                case 1: return PublisherFragment.newInstance("Publisher1", 0, "Description1", "url");
+                case 2: return PublisherFragment.newInstance("Publisher1", 0, "Description1", "url");
+                case 3: return PublisherFragment.newInstance("Publisher1", 0, "Description1", "url");
+                case 4: return PublisherFragment.newInstance("Publisher1", 0, "Description1", "url");
+                default: return PublisherFragment.newInstance("Publisher1", 0, "Description1", "url");
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return 5;
+        }
     }
 }
