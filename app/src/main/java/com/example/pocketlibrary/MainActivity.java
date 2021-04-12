7dd029434be32af278dc17b0d266aab9ca1d9424
Farzana Moury <w0737626@myscc.ca>
2021-04-12 19:11:48 -0400

    #18 Started settings     * basic layout done     * 3 menu items visible

--- app/src/main/java/com/example/pocketlibrary/MainActivity.java
+++ app/src/main/java/com/example/pocketlibrary/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pocketlibrary;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
@@ -8,6 +9,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -53,6 +55,23 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_settings: { //change username
+
+            }
+            case R.id.action_settings2:{ //change email
+
+            }
+            case R.id.action_settings3:{ //change password
+
+            }
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
