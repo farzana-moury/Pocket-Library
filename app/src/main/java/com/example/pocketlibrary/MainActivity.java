084614c09b0b728531879a925b502a067f62f74d
Farzana Moury <w0737626@myscc.ca>
2021-04-12 19:44:57 -0400

    #18 Alert Dialogs     * added for       each setting       item     * change username     * change email     * reset settings

--- app/src/main/java/com/example/pocketlibrary/MainActivity.java
+++ app/src/main/java/com/example/pocketlibrary/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.pocketlibrary;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.EditText;
 
+import com.example.pocketlibrary.database.BookDatabase;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -17,6 +23,10 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import static com.example.pocketlibrary.fragments.BooksFragment.adapter;
+import static com.example.pocketlibrary.fragments.BooksFragment.db;
+import static com.example.pocketlibrary.fragments.BooksFragment.myBooksRecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -57,17 +67,57 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.action_settings: { //change username
+                EditText username = new EditText(MainActivity.this);
+                username.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
 
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Change your username")
+                        .setIcon(R.drawable.ic_baseline_person_24)
+                        .setView(username) //the edit text where user will provide their rating
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }).setNegativeButton("CANCEL", null) //user cancels the rating action
+                        .show();
+                return true;
             }
             case R.id.action_settings2:{ //change email
+                EditText email = new EditText(MainActivity.this);
+                email.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
 
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Change your email")
+                        .setIcon(R.drawable.ic_baseline_email_24)
+                        .setView(email) //the edit text where user will provide their rating
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }).setNegativeButton("CANCEL", null) //user cancels the rating action
+                        .show();
+                return true;
             }
-            case R.id.action_settings3:{ //change password
+            case R.id.action_settings3:{ //reset ratings
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Reset ratings")
+                        .setMessage("Are you sure you want to delete your ratings? All ratings will be reset.")
+                        .setIcon(R.drawable.ic_baseline_star_24)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
 
+                            }
+                        }).setNegativeButton("CANCEL", null) //user cancels the rating action
+                        .show();
+                return true;
             }
 
         }
